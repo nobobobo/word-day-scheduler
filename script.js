9a52b95ffb1afbdb62b5ee6560ca9043b5f73fa3
nobobobo <ianhsu1221@gmail.com>
2020-04-22 22:30:13 -0700

    add js code to render timeblocks. Also added on click actions on saveBtns

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 // get and display current day 
 
 let d = new Date();
-$("#currentDay").text(d.toDateString());
+$("#currentDay").text("Today is: " + d.toDateString());
+
+// get current hour 
+
+let hr = 12;
+//d.getHours();
+
+
+// create timeblock divs
+
+for (let i = 9; i <= 17; i++) {
+    let newDiv = $("<div>").addClass("row time-block")
+    let timeElm = $("<p>").text(i + ":00").addClass("col-1 hour");
+    let textAreaElm = $("<textarea>").addClass("col-10").attr("id", i);
+    let saveBtn = $("<button>").text("Save").addClass("col-1 saveBtn").attr("btnId", i);
+
+    if (hr > i){
+        textAreaElm.addClass("past");
+    } else if (hr === i){
+        textAreaElm.addClass("present");
+    } else {
+        textAreaElm.addClass("future");
+    }
+
+    newDiv.append(timeElm, textAreaElm, saveBtn);
+    $(".container").append(newDiv);
+}
+
+
+$(document).on("click",".saveBtn", saveSchedule);
+
+
+function saveSchedule(){
+    //alert($(this).attr("btnId"));
+    //console.log($("#"+$(this).attr("btnId")))
+}
